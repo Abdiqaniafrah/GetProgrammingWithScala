d210028ce9c17473ad41265ab6f3eaaa9773b50f
Abdiqani Afrah <abdi@Abdiqanis-MacBook-Air.local>
2022-07-04 12:57:00 +0100

    classes

--- src/main/scala/myClass.scala
+++ src/main/scala/myClass.scala
@@ -1,8 +1,16 @@
 object myClass extends App {
 
   class MyClass(name: String) {
-    def sayHello(otherName: String) =
+    // This is a Class with a parameter 'name' and a method 'sayHello'
+    def sayHello(otherName: String) = {
       s"Hi $otherName, my name is $name!"
+      // 's'  is the operator for string interpolation; it replaces $otherName and $name with their value.
+    }
   }
+object MyObject {
+  val a = new MyClass("Scala")
+  // This is an instance of the class MyClass
+  println(a.sayHello("Abdi"))
+}
 
 }
